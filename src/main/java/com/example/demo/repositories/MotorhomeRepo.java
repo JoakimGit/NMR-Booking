247ae7f3513cc9394521c17fc37f797ecabd5c93
Dacho-j <joketovicdaniel@gmail.com>
2020-05-25 12:54:26 +0200

    Motorhome: Service, model, repo, controller MotorhomeHtml: Create, edit, overview

--- src/main/java/com/example/demo/repositories/MotorhomeRepo.java
+++ src/main/java/com/example/demo/repositories/MotorhomeRepo.java
@@ -1,7 +1,47 @@
 package com.example.demo.repositories;
 
+
+import com.example.demo.models.Motorhome;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class MotorhomeRepo {
+
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Motorhome> fetchAllMotorhomes() {
+        String sql = "SELECT* FROM motorhome";
+        RowMapper<Motorhome> rowMapper = new BeanPropertyRowMapper<>(Motorhome.class);
+        return template.query(sql, rowMapper);
+    }
+
+    public Motorhome fetchMotorhomeById(int motorhome_id) {
+        String sql = "SELECT * FROM motorhome WHERE motorhome_id=?";
+        RowMapper<Motorhome> rowMapper = new BeanPropertyRowMapper<>(Motorhome.class);
+        return template.queryForObject(sql, rowMapper, motorhome_id);
+    }
+
+    public void updateMotorhome(Motorhome m){
+        String sql = "UPDATE motorhome SET price=?, brand=?, model=?, available=?, bed=? WHERE motorhome_id=?";
+        template.update(sql, m.getBeds(), m.isAvailable(), m.getPrice(), m.getModel(), m.getBrand(), m.getMotorhome_id());
+    }
+
+
+    public void createMotorhome(Motorhome m) {
+        String sql = "INSERT INTO motorhome VALUES(?,?,?,?,?,?)";
+        template.update(sql, m.getMotorhome_id(), m.getBrand(), m.getModel(), m.getPrice(), m.getBeds(), m.isAvailable());
+    }
+
+    public void deleteMotorhome(int motorhome_id) {
+        String sql = "DELETE FROM item WHERE motorhome_id=?";
+        template.update(sql, motorhome_id);
+    }
+
 }
--- src/main/java/com/example/demo/services/MotorhomeService.java
+++ src/main/java/com/example/demo/services/MotorhomeService.java
@@ -1,4 +1,38 @@
 package com.example.demo.services;
 
+
+import com.example.demo.models.Motorhome;
+import com.example.demo.repositories.MotorhomeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MotorhomeService {
+    @Autowired
+    MotorhomeRepo motorhomeRepo;
+    public List<Motorhome>fetchAllMotorhomes(){
+        return motorhomeRepo.fetchAllMotorhomes();
+    }
+
+    public Motorhome fetchMotorhomeByID(int motorhome_id){
+        return motorhomeRepo.fetchMotorhomeById(motorhome_id);
+    }
+
+    public void updateMotorhome(Motorhome m){
+        motorhomeRepo.updateMotorhome(m);
+    }
+
+    public void createmotorhome(Motorhome m){
+        motorhomeRepo.createMotorhome(m);
+    }
+
+    public void deleteMotorhome(int motorhome_id){
+    motorhomeRepo.deleteMotorhome(motorhome_id);
+    }
+
+
 }
+
+
