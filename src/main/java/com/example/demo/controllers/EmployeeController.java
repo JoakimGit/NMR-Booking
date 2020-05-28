9d247a8a2fc59dba11a7089f979ed49382300442
Emil T. Madsen <emiltafteberg@protonmail.com>
2020-05-28 16:09:05 +0200

    created accessory mapping and html, translated all urls to Danish and made minor corrections to grammar.

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -21,32 +21,32 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @GetMapping("/employee/overview")
+    @GetMapping("/medarbejder/oversigt")
     public String showEmployee(Model model) {
         model.addAttribute("employees", employeeService.fetchAllEmployees());
         return "/employee/overview";
     }
 
-    @GetMapping("/employee/create")
+    @GetMapping("/medarbejder/opret")
     public String create(Model model) {
         model.addAttribute("jobs", allJobs);
         return "/employee/create";
     }
 
-    @GetMapping("employee/edit/{id}")
+    @GetMapping("medarbejder/rediger/{id}")
     public String update(@PathVariable("id") int id, Model model) {
         model.addAttribute("employee", employeeService.fetchEmployeeById(id));
         model.addAttribute("jobs", allJobs);
         return "/employee/edit";
     }
 
-    @PostMapping("/employee/edit")
+    @PostMapping("/medarbejder/rediger")
     public String updateEmployee(@ModelAttribute Employee employee) {
         employeeService.updateEmployee(employee);
         return "redirect:/employee/overview";
     }
 
-    @GetMapping("employee/delete/{id}")
+    @GetMapping("medarbejder/slet/{id}")
     public String deleteEmployee(@PathVariable("id") int id) {
         employeeService.deleteEmployee(id);
         return "redirect:/employee/overview";
